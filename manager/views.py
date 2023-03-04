262dffe2204dd06f87dcd15c9fde8a77e86c3fc6
GecataGoranov <gogogoranov04@.gmail.com>
2023-03-04 02:28:18 +0200

    Began working on the Login functionality

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .forms import LoginForm
+from .models import User
 # Create your views here.
 
 
 def index(request):
     return render(request, "manager/index.html")
+
+
+def LoginPage(request):
+    page = "login"
+    form = LoginForm()
+    return render(request, "manager/login_register.html", {"form":form})
