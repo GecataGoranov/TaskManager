a7f81902ab636941d58b672fc4a7c2bdeadf1cb1
GecataGoranov <gogogoranov04@.gmail.com>
2023-03-04 11:53:16 +0200

    Completed the front-end of the Login page

--- manager/forms.py
+++ manager/forms.py
@@ -6,5 +6,13 @@ from django.contrib.auth.forms import AuthenticationForm
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({"class":"form-control bg-secondary"})
-        self.fields["password"].widget.attrs.update({"class":"form-control bg-secondary"})
+        self.fields["username"].widget.attrs.update({"class":"form-control bg-secondary",
+                                                     "id":"username-login-input",
+                                                     "placeholder":"Username"})
+        self.fields["username"].label = "Username"
+        self.fields["password"].widget.attrs.update({"class":"form-control bg-secondary",
+                                                     "id":"password-login-input",
+                                                     "placeholder":"Password"})
+        self.fields["password"].label = "Password"
+
+
